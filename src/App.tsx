ca10ce56c9400625939d97c9c78959b68b6f6da4
稲葉 聡 <inaba_satoshi@liginc.co.jp>
2018-05-09 00:11:58 +0900

    api叩くテストの記述を削除

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,6 @@ import * as React from 'react';
 
 import Editor from 'components/Editor';
 
-import nodes from 'services/nodes';
-
-nodes.get(1)
-  .then(res => {
-    // tslint:disable-next-line:no-console
-    console.log(res.title);
-  });
-
 const App: React.SFC<any> = (props) => (
   <div>
     App
