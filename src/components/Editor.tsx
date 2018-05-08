608e63eeb5a17eb1b6ff6f9733d20c9ba7b5a298
稲葉 聡 <inaba_satoshi@liginc.co.jp>
2018-05-08 12:25:48 +0900

    testデータを追加

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -16,6 +16,21 @@ const mockList = [
     title: 'bar',
     parent_id: 2,
   },
+  {
+    id: 4,
+    title: 'hoge2',
+    parent_id: 0,
+  },
+  {
+    id: 5,
+    title: 'foo3',
+    parent_id: 6,
+  },
+  {
+    id: 6,
+    title: 'bar3',
+    parent_id: 0,
+  },
 ];
 
 // tslint:disable-next-line:max-line-length
