d9d56c7ffefbee34b2a08b537cbc12cd2bfa3fc4
稲葉 聡 <inaba_satoshi@liginc.co.jp>
2018-05-08 18:40:53 +0900

    ダミーapi用にデータを作成

--- fake-server/index.js
+++ fake-server/index.js
@@ -1,8 +1,28 @@
+const faker = require('faker');
+
 module.exports = () => {
-  const data = { users: [] }
-  // Create 1000 users
-  for (let i = 0; i < 1000; i++) {
-    data.users.push({ id: i, name: `user${i}` })
+  const data = { 
+    projects: [],
+    nodes: [] 
+  };
+  
+  const max = 50;
+  for (let i = 1; i <= max; i++) {
+    data.nodes.push({
+      id: i,
+      title: faker.name.title(),
+      parent_id: i % 3 === 0 ? 0 : faker.random.number(max),
+      project_id: faker.random.number({min: 1, max: 5}),
+    });
   }
-  return data
-}
+
+  for (let i = 1; i <= 5; i++) {
+    data.projects.push({
+      id: i,
+      title: faker.name.title(),
+      nodes: data.nodes.filter(el => el.project_id === i),
+    });
+  }
+
+  return data;
+};
