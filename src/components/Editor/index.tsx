80fbdcc6a8f7cc7b476d60e27e8b411c3c5b2ac4
稲葉 聡 <inaba_satoshi@liginc.co.jp>
2018-05-08 12:42:33 +0900

    テストデータを外部ファイル化

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,37 +1,6 @@
 import * as React from 'react';
 
-const mockList = [
-  {
-    id: 1,
-    title: 'hoge',
-    parent_id: 0,
-  },
-  {
-    id: 2,
-    title: 'foo',
-    parent_id: 1,
-  },
-  {
-    id: 3,
-    title: 'bar',
-    parent_id: 2,
-  },
-  {
-    id: 4,
-    title: 'hoge2',
-    parent_id: 0,
-  },
-  {
-    id: 5,
-    title: 'foo3',
-    parent_id: 6,
-  },
-  {
-    id: 6,
-    title: 'bar3',
-    parent_id: 0,
-  },
-];
+import mockList from 'mock/list';
 
 // tslint:disable-next-line:max-line-length
 // https://ja.stackoverflow.com/questions/7341/php%E5%A4%9A%E6%AC%A1%E5%85%83%E9%85%8D%E5%88%97%E3%81%8B%E3%82%89%E9%9A%8E%E5%B1%A4%E3%83%AA%E3%82%B9%E3%83%88%E3%82%BF%E3%82%B0ulli-liliulli-li-ul-li-ul%E3%82%92%E5%87%BA%E5%8A%9B%E3%81%97%E3%81%9F%E3%81%84/7370#7370
