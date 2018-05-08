222e7af30c5a05322306279e93c460b509649369
稲葉 聡 <inaba_satoshi@liginc.co.jp>
2018-05-08 16:30:00 +0900

    ノードにプレーンなテキストをペーストできるように実装

--- src/components/Editor/Item.tsx
+++ src/components/Editor/Item.tsx
@@ -1,18 +1,31 @@
 import * as React from 'react';
+import { compose, withHandlers } from 'recompose';
 
 interface ItemProps {
   title: string;
+  onPaste: () => void;
 }
 
-const Item: React.SFC<ItemProps> = ({ title }) => {
+const Item: React.SFC<ItemProps> = ({ title, onPaste }) => {
   return (
     <div
       suppressContentEditableWarning={true}
       contentEditable={true}
+      onPaste={onPaste}
     >
       {title}
       </div>
   );
 };
 
-export default Item;
+const enhance = compose<any, any>(
+  withHandlers({
+    onPaste: props => (e: ClipboardEvent) => {
+      e.preventDefault();
+      const value: string = e.clipboardData.getData('text/plain');
+      document.execCommand('insertHTML', false, value);
+    },
+  }),
+)(Item);
+
+export default enhance;
