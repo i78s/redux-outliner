525a6a00965c78a365dfbd216f413bc0a3ba8a1b
稲葉 聡 <inaba_satoshi@liginc.co.jp>
2018-05-10 12:32:51 +0900

    ノードのapiのCUDを叩くメソッドを追加

--- src/services/nodes.ts
+++ src/services/nodes.ts
@@ -10,4 +10,16 @@ export default {
     return instance.get(`nodes/${id}`)
       .then(res => res.data as NodeEntity);
   },
+  post(params: NodeEntity) {
+    return instance.post(`nodes`, params)
+      .then(res => res.data as NodeEntity);
+  },
+  put(params: NodeEntity) {
+    return instance.put(`nodes/${params.id}`, params)
+      .then(res => res.data as NodeEntity);
+  },
+  delete(id: number) {
+    return instance.delete(`nodes/${id}`)
+      .then(res => res.data);
+  },
 };
