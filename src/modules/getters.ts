3b638f95a5871519ca4afdb2576885287fe24525
稲葉 聡 <inaba_satoshi@liginc.co.jp>
2018-05-27 21:47:20 +0900

    テストが通るようにfindFocusNodeAfterDeleteの実装を修正

--- src/modules/getters.ts
+++ src/modules/getters.ts
@@ -47,17 +47,6 @@ export const getNodesAndReqParamBeforeCreate = (list: NodeEntity[], payload: any
 };
 // node削除後の次のフォーカスをあてるnodeを返す
 export const findFocusNodeAfterDelete = (list: NodeEntity[], target: NodeEntity): NodeEntity | null => {
-  /**
-   * 削除されたnodeに
-   *  兄弟がいない
-   *    親がいる => 親のid
-   *    親がいない => 削除できない
-   *  兄弟がいる
-   *    自身が兄弟の先頭 => 親のid
-   *    自身が兄弟の先頭じゃない
-   *      前にいる兄弟に子がいる => 自身の手前にいる兄弟のid
-   *      前にいる兄弟に子がいない => 前にいる兄弟の末尾の子のid
-   */
   let node: NodeEntity | null = null;
 
   const others = list.filter(el => el.id !== target.id);
@@ -66,35 +55,24 @@ export const findFocusNodeAfterDelete = (list: NodeEntity[], target: NodeEntity)
     .filter(el => el.parent_id === target.parent_id)
     .sort((a, b) => a.order - b.order);
 
-  if (sibling.length === 0) {
+  if (parent) {
+    node = { ...parent };
+  }
 
-    if (parent) {
-      node = { ...parent };
-    }
+  const index = target.order;
+  if (sibling.length !== 0 && index !== 0) {
+    const elder = sibling[index - 1];
+    node = { ...elder };
 
-  } else {
-
-    // todo バックエンドで削除後にorderの更新しないと不整合が起きる
-    // const index = target.order;
-    const index = list
-      .filter(el => el.parent_id === target.parent_id)
-      .sort((a, b) => a.order - b.order)
-      .findIndex(el => el.id === target.id);
-    if (index !== 0) {
-      const elder = sibling[index - 1];
-      const cousin = others
-        .filter(el => el.parent_id === elder.id)
+    let child = others
+      .filter(el => el.parent_id === elder.id)
+      .sort((a, b) => a.order - b.order);
+    while (child.length !== 0) {
+      const last = child[child.length - 1];
+      child = others
+        .filter(el => el.parent_id === last.id)
         .sort((a, b) => a.order - b.order);
-
-      node = { ...elder };
-
-      if (cousin.length !== 0) {
-        node = { ...cousin[cousin.length - 1] };
-      }
-    } else {
-      if (parent) {
-        node = { ...parent };
-      }
+      node = { ...last };
     }
   }
 
