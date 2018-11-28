a32023dcb47e2934915861483e925fc0fc8ce2bc
i78s <inaba182@gmail.com>
2018-11-29 00:27:22 +0900

    why-did-you-updateを導入

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+
 import 'reset-css';
 
 import App from '~/App';
 import store from '~/modules/store';
+
+if (process.env.NODE_ENV !== 'production') {
+  // tslint:disable-next-line:no-var-requires
+  const { whyDidYouUpdate } = require('why-did-you-update');
+  whyDidYouUpdate(React);
+}
 
 ReactDOM.render(
   <Provider store={store}>
